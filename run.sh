b8dccaf0c2d047b93715a62c3139d1a268cd329b
Zhou You <yollyhideout@gmail.com>
2016-10-06 17:30:21 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
+mkdir bin
 javac -d ./bin PrinterRunner.java
 for i in {0..9}; do
-	java -cp ./bin PrinterRunner < ./printer_input/"$i".in >| ./ans/"$i".out
+	java -cp ./bin PrinterRunner < ./printer_input/"$i".in >| ./my_ans/"$i".out
 done
 
-cmp ./ans/0.out ./printer_output/0.out
+cmp ./my_ans/0.out ./printer_output/0.out
